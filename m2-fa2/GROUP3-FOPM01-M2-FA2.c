32db2057dca635c82070d7563b0c0bd8436f7727
Dima-salang <luisgabrielle1026@gmail.com>
2025-10-15 12:04:54 +0800

    m2-fa2

--- m2-fa2/GROUP3-FOPM01-M2-FA2.c
+++ m2-fa2/GROUP3-FOPM01-M2-FA2.c
@@ -1,8 +1,6 @@
 // program for fcfs and sjf
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 
 
@@ -19,20 +17,18 @@ struct process {
 // function prototypes
 void fcfs(struct process processes[], int arr_len);
 void sjf(struct process processes[], int arr_len);
-void insert(struct process processes[], int arr_len, struct process process);
+void insert(struct process processes[], int *arr_len, struct process process);
 void min_heapify(struct process processes[], int arr_len, int index);
 void swap(struct process *process_a, struct process *process_b);
 void print_header();
 void print_averages(struct process processes[], int arr_len);
-void draw_gantt_chart(struct process processes[], int arr_len, int sum_burst_time);
+void draw_gantt_chart(struct process processes[], int arr_len, int sum_burst_time, int algo);
 int check_arrival(struct process processes[], int arr_len, int time);
 int sum_burst_time(struct process processes[], int arr_len);
 void get_user_input(struct process processes[], int *arr_len);
 struct process get_min_burst_time(struct process processes[], int *arr_len);
 
 int main() {
-
-
     int arr_len;
 
     // initialize the processes array with initial length
@@ -44,7 +40,7 @@ int main() {
     // run fcfs algo
     fcfs(processes, arr_len);
     print_averages(processes, arr_len);
-    draw_gantt_chart(processes, arr_len, sum_burst_time(processes, arr_len));
+    draw_gantt_chart(processes, arr_len, sum_burst_time(processes, arr_len), 0);
 
     // get user input for sjf
     get_user_input(processes, &arr_len);
@@ -57,7 +53,7 @@ int main() {
     // run sjf algo
     sjf(processes, arr_len);
     print_averages(processes, arr_len);
-    draw_gantt_chart(processes, arr_len, sum_burst_time(processes, arr_len));
+    draw_gantt_chart(processes, arr_len, sum_burst_time(processes, arr_len), 1);
 
     
     return 0;
@@ -160,32 +156,48 @@ void print_averages(struct process processes[], int arr_len) {
 
 
 // draw gantt chart
-void draw_gantt_chart(struct process processes[], int arr_len, int sum_burst_time) {
+// algo is:
+// 0 - fcfs
+// 1 - sjf
+void draw_gantt_chart(struct process processes[], int arr_len, int sum_burst_time, int algo) {
     // simulate the processing per second
 
+    // process index
+    int pindex = -1;
+
+    // ready queue for processes that arrive
     struct process ready_queue[arr_len];
-    struct process running_process = {0};
     int ready_queue_len = 0;
 
+    // the currently running process
+    struct process running_process = {0};
+
+    // buffer time just for drawing the gantt chart
     int buffer_time = 5;
 
     printf("\nGantt Chart\n");
     int current_time = 1;
     while (current_time <= sum_burst_time+buffer_time) {
-        // check if there is a process that arrives
-        int pindex = check_arrival(processes, arr_len, current_time);
+        // check if there is a process that arrives at current time
+        pindex = check_arrival(processes, arr_len, current_time);
         if (pindex != -1) {
             // check if there is a running process
             if (running_process.pid != 0) {
                 // put the new process in the ready queue
-                ready_queue[ready_queue_len] = processes[pindex];
-                ready_queue_len++;
+                if (algo == 0) {
+                    ready_queue[ready_queue_len] = processes[pindex];
+                    ready_queue_len++;
+                } 
+                // if it is sjf, we insert the process in the min-heap by treating the ready queue as a min-heap
+                else if (algo == 1) {
+                    insert(ready_queue, &ready_queue_len, processes[pindex]);
+                }
             } else {
                 // put the new process in the running process
                 running_process = processes[pindex];
             }
         }
-        // check for completion
+        // check for completion at curr time. and decrement the burst time
         if (running_process.pid != 0) {
             if (running_process.burst_time <= 0) {
                 struct process null_process = {0};
@@ -193,28 +205,37 @@ void draw_gantt_chart(struct process processes[], int arr_len, int sum_burst_tim
             }
             running_process.burst_time--;
         }
-        // check if there is a process in the ready queue
+        // check if there is a process in the ready queue and no running process
+        // if so, put the process in the running process
         if (ready_queue_len > 0 && running_process.pid == 0) {
             // put the process in the running process
-            running_process = ready_queue[0];
-            // remove the process from the ready queue
-            for (int i = 0; i < ready_queue_len-1; i++) {
-                ready_queue[i] = ready_queue[i+1];
+            if (algo == 0) {
+                running_process = ready_queue[0];
+                // remove the process from the ready queue
+                for (int i = 0; i < ready_queue_len-1; i++) {
+                    ready_queue[i] = ready_queue[i+1];
+                }
+                ready_queue_len--;
+            } 
+            // if it is sjf, we get the min process from the min-heap and remove it
+            else if (algo == 1) {
+                running_process = get_min_burst_time(ready_queue, &ready_queue_len);
             }
-            ready_queue_len--;
         }
 
+        // print the gantt chart
         if (running_process.pid != 0) {
             printf("| P%d ", running_process.pid);
         } else {
             printf("| * ");
         }
+
+        // increment the time
         current_time++;
     }
     printf("|");
     printf("\n");
 
-    // TO-DO: print the gantt chart for sjf
 }
 
 // helper function to check whether a process arrives at a certain time
@@ -238,7 +259,6 @@ int sum_burst_time(struct process processes[], int arr_len) {
 
 // MIN-HEAP FUNCTIONS
 
-
 // get the min from the heap and remove it
 struct process get_min_burst_time(struct process processes[], int *arr_len) {
     // get the head
@@ -254,12 +274,12 @@ struct process get_min_burst_time(struct process processes[], int *arr_len) {
     return min;
 }
 
-void insert(struct process processes[], int arr_len, struct process process) {
+void insert(struct process processes[], int *arr_len, struct process process) {
     // insert the process into the min heap
-    processes[arr_len] = process;
-    arr_len++;
+    processes[*arr_len] = process;
+    *arr_len += 1;
     
-    int index = arr_len -1;
+    int index = *arr_len -1;
     while (index > 0 && processes[(index-1)/2].burst_time > processes[index].burst_time) {
         // swap the processes
         swap(&processes[index], &processes[(index-1)/2]);
