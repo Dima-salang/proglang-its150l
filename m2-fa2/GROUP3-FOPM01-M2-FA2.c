5df0350859833efbff6ebc651ed085bb5ea243bf
Dima-salang <luisgabrielle1026@gmail.com>
2025-10-15 12:49:23 +0800

    dynamic memory alloc and getting pid and arrival time

--- m2-fa2/GROUP3-FOPM01-M2-FA2.c
+++ m2-fa2/GROUP3-FOPM01-M2-FA2.c
@@ -1,6 +1,7 @@
 // program for fcfs and sjf
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 
@@ -17,6 +18,7 @@ struct process {
 // function prototypes
 void fcfs(struct process processes[], int arr_len);
 void sjf(struct process processes[], int arr_len);
+void sort(struct process processes[], int arr_len);
 void insert(struct process processes[], int *arr_len, struct process process);
 void min_heapify(struct process processes[], int arr_len, int index);
 void swap(struct process *process_a, struct process *process_b);
@@ -31,8 +33,12 @@ struct process get_min_burst_time(struct process processes[], int *arr_len);
 int main() {
     int arr_len;
 
+    printf("\nFirst-Come First-Serve\n");
+    printf("Enter the number of processes: ");
+    scanf("%d", &arr_len);
+
     // initialize the processes array with initial length
-    struct process processes[10];
+    struct process *processes = malloc(sizeof(struct process) * arr_len);
 
     // get user input for fcfs
     get_user_input(processes, &arr_len);
@@ -42,7 +48,17 @@ int main() {
     print_averages(processes, arr_len);
     draw_gantt_chart(processes, arr_len, sum_burst_time(processes, arr_len), 0);
 
+    // free the allocated memory for fcfs
+    free(processes);
+
     // get user input for sjf
+    printf("\nShortest Job First\n");
+    printf("Enter the number of processes: ");
+    scanf("%d", &arr_len);
+
+    // reinitialize the processes array with initial length
+    processes = malloc(sizeof(struct process) * arr_len);
+
     get_user_input(processes, &arr_len);
 
     // build the min-heap from the processes
@@ -55,26 +71,33 @@ int main() {
     print_averages(processes, arr_len);
     draw_gantt_chart(processes, arr_len, sum_burst_time(processes, arr_len), 1);
 
+    // free the allocated memory for sjf
+    free(processes);
     
     return 0;
-    
 }
 
 // prompt for inputs
 void get_user_input(struct process processes[], int *arr_len) {
-    printf("Enter the number of processes: ");
-    scanf("%d", arr_len);
     
     for (int i = 0; i < *arr_len; i++) {
+        // get the pid
+        printf("Enter the pid for process %d: ", i+1);
+        scanf("%d", &processes[i].pid);
+
+        // get the burst time and arrival times
         printf("Enter the burst time for process %d: ", i+1);
         scanf("%d", &processes[i].burst_time);
-        processes[i].arrival_time = i+1;
-        processes[i].pid = i+1;
+
+        printf("Enter the arrival time for process %d: ", i+1);
+        scanf("%d", &processes[i].arrival_time);
     }
 }   
 
 // first-come first-serve function
 void fcfs(struct process processes[], int arr_len) {
+    // sort the processes by arrival time
+    sort(processes, arr_len);
     printf("\nFirst-Come First-Serve\n");
     int current_time = 1;
     print_header();
@@ -96,7 +119,6 @@ void fcfs(struct process processes[], int arr_len) {
     }
     
 }
-
 
 // shortest job first function
 void sjf(struct process processes[], int arr_len) {
@@ -135,6 +157,17 @@ void sjf(struct process processes[], int arr_len) {
         printf("%-10d %-15d %-15d %-15d %-15d %-15d\n", min_process.pid, min_process.burst_time, min_process.arrival_time, min_process.waiting_time, min_process.turnaround_time, min_process.response_time);
     }
     
+}
+
+// sort the processes by arrival time
+void sort(struct process processes[], int arr_len) {
+    for (int i = 0; i < arr_len; i++) {
+        for (int j = i+1; j < arr_len; j++) {
+            if (processes[i].arrival_time > processes[j].arrival_time) {
+                swap(&processes[i], &processes[j]);
+            }
+        }
+    }
 }
 
 
@@ -179,8 +212,8 @@ void draw_gantt_chart(struct process processes[], int arr_len, int sum_burst_tim
 
     // print legend
     printf("\nLegend:\n");
-    printf("PID: CPU is processing\n");
-    printf("* : CPU is idle\n");
+    printf("PID <time>: CPU is processing\n");
+    printf("* <time>: CPU is idle\n\n");
     int current_time = 1;
     while (current_time <= sum_burst_time+buffer_time) {
         // check if there is a process that arrives at current time
@@ -230,9 +263,10 @@ void draw_gantt_chart(struct process processes[], int arr_len, int sum_burst_tim
 
         // print the gantt chart
         if (running_process.pid != 0) {
-            printf("| P%d ", running_process.pid);
+            // print the process and the time
+            printf("| P%d %d ", running_process.pid, current_time);
         } else {
-            printf("| * ");
+            printf("| * %d ", current_time);
         }
 
         // increment the time
@@ -240,8 +274,8 @@ void draw_gantt_chart(struct process processes[], int arr_len, int sum_burst_tim
     }
     printf("|");
     printf("\n");
-
 }
+
 
 // helper function to check whether a process arrives at a certain time
 int check_arrival(struct process processes[], int arr_len, int time) {
@@ -253,6 +287,7 @@ int check_arrival(struct process processes[], int arr_len, int time) {
     return -1;
 }
 
+// sum the burst time of all processes to calculate the length of the gantt chart
 int sum_burst_time(struct process processes[], int arr_len) {
     int sum = 0;
     for (int i = 0; i < arr_len; i++) {
